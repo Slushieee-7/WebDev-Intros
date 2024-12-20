36465ba095be28bd1962103e8d35a8df15ec5605
Rey Delos Reyes <reyjoshua5922@gmail.com>
2024-12-20 21:01:45 +0800

    intro to js done

--- Introduction/intro.js
+++ Introduction/intro.js
@@ -1,1 +1,6 @@
 //This is how to comment in JavaScript
+//In JS is where all of the function is stored
+function myFunction(){
+    document.getElementById("demo").innerHTML = "button clicked!"; 
+    //"demo" is the id of the button in the html file
+}
